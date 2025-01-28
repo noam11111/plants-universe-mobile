e6075efed012825a4957b2a88a329c41c3db40f6
Ron Aruch <323725416@idf.il>
2025-01-28 16:20:22 +0200

    dependecies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,19 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.androidx.navigation.safeargs)
+    alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.androidx.room)
+    alias(libs.plugins.gms.services)
 }
 
 android {
-    namespace = "com.example.plantsuniverse"
+    namespace = "com.example.finalprojectresturant"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.plantsuniverse"
-        minSdk = 24
+        applicationId = "com.example.finalprojectresturant"
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -33,15 +37,38 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
+
+    room {
+        schemaDirectory("$projectDir/schemas")
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.gridlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.annotation)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.firebase.ui.auth)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.runtime)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.retrofit.retrofit)
+    implementation(libs.retrofit.gsonConverter)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.firestore.ktx)
+    implementation("com.github.bumptech.glide:glide:4.15.1") // Glide library
+    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1") // Glide compiler
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath(libs.androidx.navigation.safe.args.gradle.plugin)
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.ksp) apply false
+    alias(libs.plugins.androidx.room) apply false
+    alias(libs.plugins.gms.services) apply false
 }
